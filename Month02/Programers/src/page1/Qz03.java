09c08f20b38c5276a540b90f06f4a471e6018629
MinChangsu <mgs1527@icloud.com>
2022-11-07 22:42:54 +0900

    Create Qz03.java

---
+++ Month02/Programers/src/page1/Qz03.java
@@ -0,0 +1,20 @@
+package page1;
+//정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때
+//, array에 n이 몇 개 있는 지를 return 하도록 solution
+//함수를 완성해보세요.
+
+public class Qz03 {
+	public int solution(int[] array, int n) {
+        int answer = 0;
+        for(int i:array) {
+        	if(i==n)
+        		answer++;
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		
+
+	}
+
+}
